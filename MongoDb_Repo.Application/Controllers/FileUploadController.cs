7deccb7c491c1a74a4aeebdb0f6a4f49440d3177
Quan Nguyen <quan.nguyen@vault22.io>
2024-07-30 15:54:57 +0700

    Test commit

---
+++ MongoDb_Repo.Application/Controllers/FileUploadController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MongoDb_Repo.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost("Evaluation")]
+        public async Task<IActionResult> UploadEvaluation(List<IFormFile> files)
+        {
+            return Ok();
+        }
+    }
+}
