9f901d298d77a5a3fda9f785b8f865fb9404d974
Quan Nguyen <quan.nguyen@vault22.io>
2024-07-30 16:45:17 +0700

    Add new folders

--- MongoDb_Repo.Application/Controllers/FileUploadController.cs
+++ MongoDb_Repo.Application/Controllers/FileUploadController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDb_Repo.Domain.Interface;
 
 namespace MongoDb_Repo.Application.Controllers
 {
@@ -7,9 +8,18 @@ namespace MongoDb_Repo.Application.Controllers
     [ApiController]
     public class FileUploadController : ControllerBase
     {
+
+        private readonly IFileUploadService _uploadService;
+
+        public FileUploadController(IFileUploadService uploadService)
+        {
+            _uploadService = uploadService;
+        }
+
         [HttpPost("Evaluation")]
         public async Task<IActionResult> UploadEvaluation(List<IFormFile> files)
         {
+            await _uploadService.HandleEvaluationFiles(files.Where(f=>f.Length>0).Select(f => f.OpenReadStream())); 
             return Ok();
         }
     }
